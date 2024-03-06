fb41fb92b5a8fb634fc7d7cb9f9bf6cdd4a108d0
MilewskiMaciej <milewskimaciej@o2.pl>
2024-03-06 16:05:40 +0100

    commit

--- lab_3.py
+++ lab_3.py
@@ -15,10 +15,28 @@ def zad2():
     print(lista1)
     nowa_lista = [j for j in lista1 if j%2 == 0]
     print(nowa_lista)
+
+#Zadanie 3
+def zad3():
+    produkty = {'jajka': 7, 'maka': '3kg', 'bulki': 5, 'cukier': '1kg'}
+    lista = []
+
+#Zadanie 4
+def zad4():
+    a = int(input("Podaj dlugosc boku a: "))
+    b = int(input("Podaj dlugosc boku b: "))
+    c = int(input("Podaj dlugosc boku c: "))
+    if a < c and b < c:
+        if a*a+b*b==c*c:
+            print("Jest to trójkąt prostokątny")
+        else:
+            print("Nie jest to trójkąt prostokątny")
+    else:
+        print("Podano złe długości")
 """
 
 def main():
 #    zad1()
 #    zad2()
-
+#    zad4()
 main()
