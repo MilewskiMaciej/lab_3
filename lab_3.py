26f5e3f5e4f36ed1c63a7d6b86ea1b8de0167076
MilewskiMaciej <milewskimaciej@o2.pl>
2024-03-06 15:41:48 +0100

    commit

--- lab_3.py
+++ lab_3.py
@@ -1,8 +1,8 @@
 #Zadanie 1
 def zad1():
-    a = set(i for i in range(1,11))
-    b = set(4**j for j in range(0,8))
-    c = set(k for k in b if k%2==0)
+    a = [i for i in range(1,11)]
+    b = [4**j for j in range(0,8)]
+    c = [k for k in b if k%2==0]
     print(a)
     print(b)
     print(c)
