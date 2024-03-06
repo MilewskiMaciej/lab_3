54ab2bf84447b3385ce2286a3fe319c05b1c4ed7
MilewskiMaciej <milewskimaciej@o2.pl>
2024-03-06 15:50:12 +0100

    commit

--- lab_3.py
+++ lab_3.py
@@ -1,4 +1,6 @@
+import random
 #Zadanie 1
+"""
 def zad1():
     a = [i for i in range(1,11)]
     b = [4**j for j in range(0,8)]
@@ -6,9 +8,17 @@ def zad1():
     print(a)
     print(b)
     print(c)
-
+    
+#Zadanie 2
+def zad2():
+    lista1 = [random.randint(1, 15) for i in range(10)]
+    print(lista1)
+    nowa_lista = [j for j in lista1 if j%2 == 0]
+    print(nowa_lista)
+"""
 
 def main():
-    zad1()
+#    zad1()
+#    zad2()
 
 main()
